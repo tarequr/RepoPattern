8414eec9f2d960ec326ae033181a1ab88c5105c3
tarequr <tarequrr8@gmail.com>
2023-04-11 22:11:29 +0600

    migration

---
+++ app/Repositories/PostRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Interfaces\PostRepositoryInterface;
+use App\Models\Post;
+
+class PostRepository implements PostRepositoryInterface
+{
+    public function allPosts()
+    {
+        return Post::orderBy('id', 'desc')->get();;
+    }
+
+    public function storePost($data)
+    {
+        return Post::create($data);
+    }
+
+    public function findPost($id)
+    {
+        return Post::find($id);
+    }
+
+    public function updatePost($data, $id)
+    {
+        $Post = Post::where('id', $id)->first();
+        $Post->name = $data['name'];
+        $Post->slug = $data['slug'];
+        $Post->save();
+    }
+
+    public function destroyPost($id)
+    {
+        $Post = Post::find($id);
+        $Post->delete();
+    }
+}
