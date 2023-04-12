95dcabe9421254768b4759a2a8d3cebc9b18f32c
tarequr <tarequrr8@gmail.com>
2023-04-12 20:58:05 +0600

    migration ready

--- database/migrations/2023_04_11_155640_create_posts_table.php
+++ database/migrations/2023_04_11_155640_create_posts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->date('publication_date')->nullable();
+            $table->boolean('status')->default(0);
             $table->timestamps();
         });
     }
